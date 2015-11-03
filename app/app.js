5ebe9918fcfbed53e4042a8637e30519b565c875
Blake Hagen <blakehagen@gmail.com>
2015-11-03 12:24:24 -0700

    just started step 2

--- app/app.js
+++ app/app.js
@@ -1,5 +1,17 @@
-var app = angular.module('directivePractice', ['ui.route']);
+var app = angular.module('directivePractice', ['ui.router']);
 
-app.config(function(){
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'homeTmpl.html',
+            controller: 'homeCtrl'
+        })
+
+
+    $urlRouterProvider
+        .otherwise('/home');
+
 
 });
---
+++ app/directives.js
@@ -0,0 +1,13 @@
+angular.module('directivePractice').directive('dirDisplay', function () {
+
+    return {
+        templateUrl: 'app/dirDisplayTmpl.html',
+        link: function(scope, elem, attrs){
+            
+        }
+    }
+
+
+
+
+});
